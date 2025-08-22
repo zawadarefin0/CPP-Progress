1a77e2cfbac8c6bf4a0fff6d0d881e078805018e
Your Name <zawadarefin0@gmail.com>
2025-08-22 15:58:09 +1000

    ts broken

--- login.cpp
+++ login.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 using namespace std;
 
-
-int choice;
 bool loggedIn = false;
+int choice;
 
-void loginMenu();
+void loginMenu() {
+    if (!loggedIn) {
+        choice = 0;
+        cout << "You are not logged in!" << endl;
+        cout << "1. Register \n2. Login \n3. Forgot Password" << endl; cin >> choice;
+    } else {
+        cout << "You are already logged in!" << endl;
+    }
+};
+
+void mainMenu() {
+    cout << "This is the main menu" << endl;
+}
+
 
 int main() {
-    cout << choice << endl;
-    vector<int> ids = {};
+    // system("clear");
     vector<string> usernames = {};
     vector<string> passwords = {};
 
@@ -20,25 +32,56 @@ int main() {
 
     string loginUsername;
     string loginPassword;
+    
 
-    switch (choice) {
+    while (true) {
+
+        if (!loggedIn) {
+            loginMenu();
+        } else {
+            mainMenu();
+            break;
+        }
+    }
+
+    
+        switch (choice) {
         case 1:
+            // system("clear");
             cout << "Enter your username: "; cin >> registerUsername;
             cout << "Enter your password: "; cin >> registerPassword;
 
             usernames.push_back(registerUsername);
             passwords.push_back(registerPassword);
 
-        
-    }
-    loginMenu();
-}
+            // system("clear");
+            cout << "Account registration successful." << endl;
+            loginMenu();
+            break;
+            
 
-void loginMenu() {
-    if (loggedIn == false) {
-        cout << "You are not logged in!" << endl;
-        cout << "1. Register \n2. Login \n3. Forgot Password" << endl; cin >> choice;
-    } else {
-        cout << "You are already logged in!" << endl;
-    }
-};
+        case 2:
+            // system("clear");
+            cout << "Enter your username: "; cin >> loginUsername;
+            cout << "Enter your password: "; cin >> loginPassword;
+
+            
+            for (int i = 0; i <= (usernames.size() - 1); i++) {
+                cout << usernames.size() - 1 << endl;
+                if (loginUsername == usernames[i] && loginPassword == passwords[i]) {
+                    cout << "i = " << i << endl << "usernames[i] = " << usernames[i] << endl << "passwords[i] = " << passwords[i] << endl;
+                    loggedIn = true;
+                    // system("clear");
+                    cout << "You have successfully logged in!" << endl << endl;
+                    mainMenu();
+                    break;
+                }
+                    
+            }
+
+            cout << "You have entered your credentials incorrectly." << endl << endl;
+            break;
+
+            }
+    
+        }
