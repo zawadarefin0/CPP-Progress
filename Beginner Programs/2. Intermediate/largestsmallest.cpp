cc85e18f771ac4f615dc4cc2c63b810eb23a3ea8
Your Name <zawadarefin0@gmail.com>
2025-08-23 20:46:30 +1000

    finished

--- Beginner Programs/2. Intermediate/largestsmallest.cpp
+++ Beginner Programs/2. Intermediate/largestsmallest.cpp
@@ -3,14 +3,23 @@ using namespace std;
 
 int main() {
 
-    int array[] = {5, 8, 3, 4, 1, 9, 10, 7, 6, 2 };
-    int smallest;
-    int largest;
+    int array[] = { 5, 8, 3, 4, 1, 9, 10, 7, 6, 2 };
+    int smallest = array[0];
+    int largest = array[0];
     int arrayLength = sizeof(array) / sizeof(array[0]);
     for (int i = 0; i <= arrayLength - 1; i++) {
         int x = array[i];
-        if (x )
+        int y = array[i];
+        if (x <= smallest && array[(i + 1) != 10]) {
+            smallest = x;
+        };
+        if (y >= largest && array[(i+1) != 10]) {
+            largest = y;
+        }
     }
+
+    cout << smallest << endl;
+    cout << largest << endl;
 
 
     return 0;
