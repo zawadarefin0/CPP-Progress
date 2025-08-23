e44d6494c2aa0ccb950032f011ab1523b0f9cef3
Your Name <zawadarefin0@gmail.com>
2025-08-23 20:21:35 +1000

    initialised intermediate programs

---
+++ Beginner Programs/2. Intermediate/largestsmallest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int array[] = {5, 8, 3, 4, 1, 9, 10, 7, 6, 2 };
+    int smallest;
+    int largest;
+    int arrayLength = sizeof(array) / sizeof(array[0]);
+    for (int i = 0; i <= arrayLength - 1; i++) {
+        int x = array[i];
+        if (x )
+    }
+
+
+    return 0;
+}
